29f04b7bf6b40fa7f5d248fad05c20ce5794413a
crysthianophp <crysthianophp@gmail.com>
2014-11-11 01:01:46 -0200

    criando o cadastro de usuarios

--- module/Core/src/Core/Form/CadastroUsersForm.php
+++ module/Core/src/Core/Form/CadastroUsersForm.php
@@ -20,7 +20,7 @@ class CadastroUsersForm extends Form
             'attributes' => array(
                 'class' => 'form-control input-sm',
                 'placeholder' => 'Digite Seu Nome.',
-                'style' => 'width: 300px;'
+                'style' => 'width: 500px;'
             )
         ));
 
@@ -33,7 +33,88 @@ class CadastroUsersForm extends Form
             'attributes' => array(
                 'class' => 'form-control input-sm',
                 'placeholder' => 'Digite seu nome de Usuário.',
-                'style' => 'width: 300px'
+                'style' => 'width: 500px'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'email',
+            'type' => 'email',
+            'options' => array(
+                'label' => 'Email'
+            ),
+            'attributes' => array(
+                'class' => 'form-control input-sm',
+                'placeholder' => 'email@email.com.br',
+                'style' => 'width: 500px;'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'password',
+            'type' => 'password',
+            'options' => array(
+                'label' => 'Senha'
+            ),
+            'attributes' => array(
+                'class' => 'form-control input-sm',
+                'placeholder' => 'Digite uma senha',
+                'style' => 'width: 500px;'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'password_confirm',
+            'type' => 'password',
+            'options' => array(
+                'label' => 'Confirmar senha'
+            ),
+            'attributes' => array(
+                'class' => 'form-control input-sm',
+                'placeholder' => 'Confirme a sua senha',
+                'style' => 'width: 500px;'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'roles',
+            'type' => 'select',
+            'attributes' => array(
+                'multiple' => 'multiple',
+                'class' => 'form-control input-sm',
+                'placeholder' => 'Selecione um Papel',
+                'style' => 'width: 500px; height: 100px;'
+            ),
+            'options' => array(
+                'label' => 'Papel',
+                //'empty_option' => '',
+                /*'value_options' => array(
+                    '0' => 'teste 1',
+                    '1' => 'teste 2'
+                )*/
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'state',
+            'type' => 'checkbox',
+            'options' => array(
+                'label' => 'Ativo',
+                'label_attributes' => array(
+                    'style' => 'text-indent: 4px;'
+                )
+            ),
+            'attributes' => array(
+                'style' => 'vertical-align: -4px;'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'salvar',
+            'type' => 'submit',
+            'attributes' => array(
+                'value' => 'Salvar',
+                'class' => 'btn btn-primary'
             )
         ));
     }
